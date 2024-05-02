bdb8ec5fb6b2ef31421d4e7a8b4ff5677c9ee8ca
RD-Code <hiralal.sandeep@gmail.com>
2024-05-01 19:09:52 -0800

    try to run properties

--- rd.js
+++ rd.js
@@ -5,8 +5,11 @@ function add(num){
    if (score>num) {
      console.log("The score is greater than number.");
    }
+   else if(score===num){
+    console.log("The Score is equal to the Number");
+   }
    else{
     console.log("The score is less than number");
    }
 }
-(add(400));
+(add(1000));
