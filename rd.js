a27e67de4ae3656c587833e85833bc7a0f1580f9
RD-Code <hiralal.sandeep@gmail.com>
2024-05-01 19:01:49 -0800

    summary

---
+++ rd.js
@@ -0,0 +1,12 @@
+// ##### Control Flow ########
+// ****** 1) Conditional Statements **********
+const score = 1000;
+function add(num){
+   if (score>num) {
+     console.log("The score is greater than number.");
+   }
+   else{
+    console.log("The score is less than number");
+   }
+}
+(add(400));
